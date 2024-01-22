3d11a47f83599561dac24a72288ff600cc6e3a3a
789363 <bike851013gmail.com>
2024-01-22 09:26:38 +0800

    Feat:UploadSwagger

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Post, UseInterceptors, UploadedFile, Body } from '@nestjs/common';
+import { Controller, Post, UseInterceptors, UploadedFile } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiConsumes, ApiBody, ApiTags, ApiResponse } from '@nestjs/swagger';
 import { LocalStorageService } from './upload.servie';
 
+@ApiTags('files')
 @Controller('files')
 export class FileUploadController {
     constructor(private readonly localStorageService: LocalStorageService) { }
 
     @Post('upload')
     @UseInterceptors(FileInterceptor('file'))
+    @ApiConsumes('multipart/form-data')
+    @ApiBody({
+        description: 'Upload a file',
+        type: FileUploadDto,
+    })
+    @ApiResponse({ status: 200, description: 'File uploaded successfully' })
     async uploadFile(@UploadedFile() file: Express.Multer.File) {
         const fileUrl = await this.localStorageService.saveFile(file);
         return {
@@ -15,4 +23,8 @@ export class FileUploadController {
             url: fileUrl,
         };
     }
+}
+
+class FileUploadDto {
+    file: any;
 }
