83cb8cbfc6d5dfaf9f8fe0cc3c0a869865d2a80a
789363 <bike851013gmail.com>
2024-01-23 10:45:04 +0800

    Feat:DeleteNoticeLogic

--- src/notice/notice.service.ts
+++ src/notice/notice.service.ts
@@ -1,25 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
 import { CreateCollectionNoticeDto, UpdateCollectionNoticeDto, CreateTenementNoticeDto, UpdateTenementNoticeDto, NoticeDto } from './dto/notice.dto';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class NoticeService {
     constructor(private prisma: PrismaService) { }
 
     async getNoticeByIdAndType(id: number, type: string) {
-        let notice;
+        let notices;
         if (type === 'collection') {
-            notice = await this.prisma.collection_Notice.findUnique({
-                where: { id },
+            notices = await this.prisma.collection_Notice.findMany({
+                where: {
+                    collection_id: id,
+                }
             });
         } else {
-            notice = await this.prisma.tenement_Notice.findUnique({
-                where: { id },
+            notices = await this.prisma.tenement_Notice.findMany({
+                where: {
+                    tenement_id: id,
+                },
             });
         }
 
-        return notice ? { message: "success", data: [notice] } : { message: "Notice not found" };
+        return notices.length > 0
+            ? { message: "success", data: notices }
+            : { message: "Notice not found" };
     }
+
 
     async createNotice(type: string, noticeData: CreateCollectionNoticeDto | CreateTenementNoticeDto) {
         let newNotice;
@@ -54,16 +62,27 @@ export class NoticeService {
     }
 
     async deleteNotice(id: number, type: string) {
-        if (type === 'collection') {
-            await this.prisma.collection_Notice.delete({
-                where: { id },
-            });
-        } else {
-            await this.prisma.tenement_Notice.delete({
-                where: { id },
-            });
-        }
+        try {
+            if (type === 'collection') {
+                await this.prisma.collection_Notice.delete({
+                    where: { id },
+                });
+            } else {
+                await this.prisma.tenement_Notice.delete({
+                    where: { id },
+                });
+            }
 
-        return { message: "notices deleted" };
+            return { message: "notices deleted" };
+        } catch (error) {
+            // 捕獲 Prisma 抛出的特定錯誤，例如試圖刪除不存在的資源
+            if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2025') {
+                throw new NotFoundException(`Notice with ID ${id} not found`);
+            }
+
+            // 如果有其他未知錯誤，可以重新抛出或處理
+            throw error;
+        }
     }
+
 }
