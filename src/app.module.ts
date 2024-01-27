3293c18f116c7c1929483f212c9b11c0a95a14ce
789363 <40884505+789363@users.noreply.github.com>
2024-01-27 14:20:42 +0800

    Feat:AutoCreateAdminByFristRunStart

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 // src/app.module.ts
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service'; // 确保导入 UsersService
 import { AuthModule } from './auth/auth.module';
 import { CollectionService } from './collection/collection.service';
 import { CollectionController } from './collection/collection.controller';
@@ -21,6 +22,7 @@ import { NoticeController } from './notice/notice.controller';
 import { CalendarController } from './calendar/calendar.controller';
 import { CalendarService } from './calendar/calendar.service';
 import { LocalStorageService } from './upload/LocalStorageService';
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     UsersModule,
@@ -32,6 +34,9 @@ import { LocalStorageService } from './upload/LocalStorageService';
     }),
     TenementModule,
     CalendarModule,
+    ConfigModule.forRoot({
+      isGlobal: true, // 使配置全局可用
+    }),
   ],
   controllers: [
     AppController,
@@ -50,6 +55,13 @@ import { LocalStorageService } from './upload/LocalStorageService';
     NoticeService,
     CalendarService,
     LocalStorageService,
+    UsersService, // 确保 UsersService 被注入
   ],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  constructor(private usersService: UsersService) {} // 注入 UsersService
+
+  async onModuleInit() {
+    await this.usersService.ensureAdminUser(); // 在模块初始化时确保管理员账户存在
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,11 +4,22 @@ import * as bcrypt from 'bcrypt';
 import { GetUserListDto } from './dto/get-userlist.dto';
 import { User } from '@prisma/client';
 import { UserData } from './interface/user.interface';
+import { ConfigService } from '@nestjs/config'; // 引入 ConfigService
+
 @Injectable()
 export class UsersService {
-  constructor(private prisma: PrismaService) { }
+  constructor(
+    private prisma: PrismaService,
+    private configService: ConfigService,
+  ) {}
 
-  async createUser(createUserDto: { user_name: string; user_email: string; user_password: string; isadmin: boolean, isDelete: boolean }): Promise<UserData> {
+  async createUser(createUserDto: {
+    user_name: string;
+    user_email: string;
+    user_password: string;
+    isadmin: boolean;
+    isDelete: boolean;
+  }): Promise<UserData> {
     const existingUser = await this.prisma.user.findUnique({
       where: {
         user_email: createUserDto.user_email,
@@ -27,7 +38,7 @@ export class UsersService {
         user_password: hashedPassword,
         status: true, // 根据业务逻辑设置默认状态
         isAdmin: createUserDto.isadmin, // 使用传入的值
-        isDeleted: createUserDto.isDelete
+        isDeleted: createUserDto.isDelete,
       },
       select: {
         user_id: true,
@@ -38,13 +49,13 @@ export class UsersService {
         isDeleted: true,
         user_password: false,
         // 注意：不返回 user_password 字段
-      }
+      },
     });
 
     return {
       ...user,
       isadmin: user.isAdmin,
-      isDelete: user.isDeleted
+      isDelete: user.isDeleted,
     };
   }
 
@@ -73,7 +84,10 @@ export class UsersService {
       throw new Error('User not found');
     }
     if (updateData.user_password) {
-      updateData.user_password = await bcrypt.hash(updateData.user_password, 10);
+      updateData.user_password = await bcrypt.hash(
+        updateData.user_password,
+        10,
+      );
     }
 
     return this.prisma.user.update({
@@ -87,7 +101,7 @@ export class UsersService {
   async deleteUser(userId: number): Promise<User> {
     return this.prisma.user.update({
       where: {
-        user_id: userId
+        user_id: userId,
       },
       data: {
         isDeleted: true,
@@ -123,7 +137,7 @@ export class UsersService {
         status: true,
         isAdmin: true,
         isDeleted: true,
-      }
+      },
     });
 
     if (!user) {
@@ -133,7 +147,29 @@ export class UsersService {
     return user;
   }
 
+  async ensureAdminUser() {
+    const adminName = 'admin';
+    const adminEmail = this.configService.get<string>('ADMIN_EMAIL');
+    const adminPassword = this.configService.get<string>('ADMIN_PASSWORD');
 
+    // 检查是否存在管理员用户
+    const adminExists = await this.prisma.user.findUnique({
+      where: {
+        user_email: adminEmail,
+      },
+    });
+    // 加密密码
+    const hashedPassword = await bcrypt.hash(adminPassword, 10);
+    if (!adminExists) {
+      await this.prisma.user.create({
+        data: {
+          user_name: adminName,
+          user_email: adminEmail,
+          user_password: hashedPassword,
+          isAdmin: true,
+          status: false,
+        },
+      });
+    }
+  }
 }
-
-
